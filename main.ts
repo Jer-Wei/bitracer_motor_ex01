af517f84bf170ff23fcd020ac217e4d4473da795
Jer-Wei Rau <Jer-Wei@users.noreply.github.com>
2020-08-26 15:47:57 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,44 +1,24 @@
 input.onButtonPressed(Button.A, function () {
-    music.playTone(262, music.beat(BeatFraction.Whole))
-    Motor(2, 0, 200)
+	
 })
-function MotorRun (ID: number,Dㄐㄛ Speed: number): void {
+input.onButtonPressed(Button.B, function () {
+	
+})
+function MotorRun (Motot_ID: number, Motot_Speed: number) {
     let i2cbuf = pins.createBuffer(3);
-    if (ID == 0) {
+    if (Motot_ID == 0) {
         i2cbuf[0] = 0x00;
-    } else {
+        i2cbuf[1] = Motot_Speed >> 8;
+        i2cbuf[2] = Motot_Speed;
+    } 
+    if (Motot_ID == 1){
         i2cbuf[0] = 0x02;
-    }
-    if (Speed => 0) {
-        i2cbuf[1] = 0;
-    } else {
-        i2cbuf[1] = 1;
-    }
-    if 
-    i2cbuf[2] = Speed
-    pins.i2cWriteBuffer(0x10, i2cbuf)
-}
 
-    export function motorRun(index: Motors, PWM: number): void {
-        let i2cbuf = pins.createBuffer(3);
-        if (index == 0) {
-            i2cbuf[0] = 0x02;
-            i2cbuf[1] = PWM >> 8;
-            i2cbuf[2] = PWM;
-            pins.i2cWriteBuffer(N76_ADDR, i2cbuf);
-        }
-        if (index == 1) {
-            i2cbuf[0] = 0x00;
-            i2cbuf[1] = PWM >> 8;
-            i2cbuf[2] = PWM;
-            pins.i2cWriteBuffer(N76_ADDR, i2cbuf);
-        }
-        if (index == 2) {	
-            i2cbuf[0] = 0x00;
-            i2cbuf[1] = PWM >> 8;
-            i2cbuf[2] = PWM;
-            pins.i2cWriteBuffer(N76_ADDR, i2cbuf);
-            i2cbuf[0] = 0x02;
-            pins.i2cWriteBuffer(N76_ADDR, i2cbuf);
-        }
+    } 
+    ㄑㄛ {
+
     }
+    i2cbuf[1] = Dir
+    i2cbuf[2] = Motot_Speed
+    pins.i2cWriteBuffer(0x10, i2cbuf);
+}
