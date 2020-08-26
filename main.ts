1de36b88ee6230a3123874f78884c46d42182b22
Jer-Wei Rau <Jer-Wei@users.noreply.github.com>
2020-08-26 23:26:31 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,32 +1,38 @@
+let mspeed = 0
 input.onButtonPressed(Button.A, function () {
-    MotorRun(0, 1000)
+    MotorRun(1, 200)
 })
 input.onButtonPressed(Button.AB, function () {
     MotorRun(2, 0)
 })
 input.onButtonPressed(Button.B, function () {
-    MotorRun(1, -1000)
+    let i2cbuf = pins.createBuffer(3);
+    let mspeed = 50
+    i2cbuf[0] = 0
+    i2cbuf[1] = 1
+    i2cbuf[2] = mspeed
+    pins.i2cWriteBuffer(0x10, i2cbuf)
 })
 function MotorRun (Motot_ID: number, Motot_Speed: number) {
-    let i2cbuf = pins.createBuffer(3);
-if (Motot_ID == 0) {
-        i2cbuf[0] = 0
-        i2cbuf[1] = Motot_Speed >> 8
-        i2cbuf[2] = Motot_Speed
-        pins.i2cWriteBuffer(0x10, i2cbuf)
+    let i2cbuf2 = pins.createBuffer(3);
+    if (Motot_ID == 0) {
+        i2cbuf2[0] = 0x00
+        i2cbuf2[1] = Motot_Speed >> 8
+        i2cbuf2[2] = Motot_Speed
+        pins.i2cWriteBuffer(0x10, i2cbuf2)
     }
     if (Motot_ID == 1) {
-        i2cbuf[0] = 2
-        i2cbuf[1] = Motot_Speed >> 8
-        i2cbuf[2] = Motot_Speed
-        pins.i2cWriteBuffer(0x10, i2cbuf)
+        i2cbuf2[0] = 0x02
+        i2cbuf2[1] = Motot_Speed >> 8
+        i2cbuf2[2] = Motot_Speed
+        pins.i2cWriteBuffer(0x10, i2cbuf2)
     }
     if (Motot_ID == 2) {
-        i2cbuf[0] = 0
-        i2cbuf[1] = Motot_Speed >> 8
-        i2cbuf[2] = Motot_Speed
-        pins.i2cWriteBuffer(0x10, i2cbuf)
-        i2cbuf[0] = 2
-        pins.i2cWriteBuffer(0x10, i2cbuf)
+        i2cbuf2[0] = 0
+        i2cbuf2[1] = Motot_Speed >> 8
+        i2cbuf2[2] = Motot_Speed
+        pins.i2cWriteBuffer(0x10, i2cbuf2)
+i2cbuf2[0] = 2
+        pins.i2cWriteBuffer(0x10, i2cbuf2)
     }
 }
