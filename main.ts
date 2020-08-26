64238424c831569749cc8bd295c36fee1d7c25f1
Jer-Wei Rau <Jer-Wei@users.noreply.github.com>
2020-08-26 16:17:23 +0800

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -12,13 +12,14 @@ function MotorRun (Motot_ID: number, Motot_Speed: number) {
 if (Motot_ID == 0) {
         i2cbuf[0] = 0
         i2cbuf[1] = Motot_Speed >> 8
-        serial.writeNumbers(i2cbuf)
+        serial.writeNumber(i2cbuf[1])
         i2cbuf[2] = Motot_Speed
         pins.i2cWriteBuffer(0x10, i2cbuf)
     }
     if (Motot_ID == 1) {
         i2cbuf[0] = 2
         i2cbuf[1] = Motot_Speed >> 8
+        serial.writeNumber(i2cbuf[1])
         i2cbuf[2] = Motot_Speed
         pins.i2cWriteBuffer(0x10, i2cbuf)
     }
